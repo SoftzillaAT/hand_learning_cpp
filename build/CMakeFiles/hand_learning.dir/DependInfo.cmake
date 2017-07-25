1e2ebb1648cc42e1323b060e78f654cad112e7df
Dominik Streicher <office@softzilla.at>
2017-07-25 13:07:16 +0200

    init

---
+++ build/CMakeFiles/hand_learning.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dom/diplomata/hand_learning/cpp/hand_learning/main.cpp" "/home/dom/diplomata/hand_learning/cpp/hand_learning/build/CMakeFiles/hand_learning.dir/main.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "DISABLE_PCAP"
+  "DISABLE_PNG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  "/usr/include/vtk-5.8"
+  "../include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/build"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/3rdparty"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/3rdparty/pcl_1_8"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/3rdparty/SiftGPU/src/siftgpu_external/src"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/core/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/io/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/common/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/keypoints/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/ml/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/rendering/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/segmentation/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/attention_segmentation/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/change_detection/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/features/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/reconstruction/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/registration/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/tracking/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/object_modelling/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/recognition/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/apps/include"
+  "/home/dom/diplomata/install/v4r_hannes/v4r/modules/camera_tracking_and_mapping/include"
+  "/usr/include/pcl-1.7"
+  "/usr/include/eigen3"
+  "/usr/include/ni"
+  "/usr/include/openni2"
+  "/usr/include/libusb-1.0"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/start.sh
@@ -0,0 +1,1 @@
+./hand_learning -i /home/dom/diplomata/images/loop4/ -p ".*.pcd" -d 1 -m mesh.ply -c cloud7.pcd
